985377ec55d05712fb2eebfcc7472d389248dc54
MaxSocco <maxime.soccodato@okargo.com>
2021-09-09 17:14:33 +0200

    add get

--- lib/main.dart
+++ lib/main.dart
@@ -29,10 +29,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  void _incrementCounter() {
-    setState(() {});
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
